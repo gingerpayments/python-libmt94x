487fbece18d82353e7118503fd8cf7b78b010b10
Martin Matusiak <numerodix@gmail.com>
2016-01-25 13:53:50 +0100

    implement tm94x serializer

---
+++ ginger/libmt94x/serializer.py
@@ -0,0 +1,66 @@
+import re
+
+builtin_type = type
+
+
+class Tm94xSerializer(object):
+    TYPE_CHARACTER = 1
+    TYPE_NUMERIC = 2
+
+    swift_charset_chars = (
+        b"abcdefghijklmnopqrstuvwxyz"
+        b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        b"0123456789"
+        b"/-?().,+'{}: "
+    )
+    swift_charset_numbers = b"0123456789"
+
+    # Creates a pattern that must match the whole string where a byte can be
+    # matched any number of times
+    rx_chars = re.compile(
+        '^' +
+        '(?:' +
+        '|'.join([re.escape(c) for c in swift_charset_chars]) + 
+        ')*' +
+        '$'
+    )
+    rx_nums = re.compile(
+        '^' +
+        '(?:' +
+        '|'.join([re.escape(c) for c in swift_charset_numbers]) + 
+        ')*' +
+        '$'
+    )
+
+
+    # Convenience properties
+
+    @property
+    def type_char(self):
+        return self.TYPE_CHARACTER
+
+    @property
+    def type_num(self):
+        return self.TYPE_NUMERIC
+
+
+    # Public API
+
+    def serialize_value(self, type, maxlen, value):
+        # Even if the value represents a number it could have leading zeros, so
+        # we manipulate it as a bytestring
+        if builtin_type(value) != bytes:
+            raise ValueError("Must pass a bytestring")
+
+        if len(value) > maxlen:
+            raise ValueError("Value cannot exceed %s bytes" % maxlen)
+
+        if type == self.TYPE_CHARACTER and not self.rx_chars.match(value):
+            raise ValueError("Character string value can only contain the bytes: %s"
+                             % self.swift_charset_chars)
+
+        if type == self.TYPE_NUMERIC and not self.rx_nums.match(value):
+            raise ValueError("Numeric value can only contain the bytes: %s"
+                             % self.swift_charset_numbers)
+
+        return value
---
+++ ginger/libmt94x/tests/test_serializer.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+
+from ginger.libmt94x.serializer import Tm94xSerializer
+
+
+class SerializerTests(TestCase):
+    def setUp(self):
+        self.ser = Tm94xSerializer()
+
+    # Character tests
+
+    def test_char_charset_lowercase(self):
+        lowercase = b'abcdefghijklmnopqrstuvwxyz'
+        val = self.ser.serialize_value(self.ser.type_char, len(lowercase), lowercase)
+        self.assertEquals(val, lowercase)
+
+    def test_char_charset_uppercase(self):
+        uppercase = b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        val = self.ser.serialize_value(self.ser.type_char, len(uppercase), uppercase)
+        self.assertEquals(val, uppercase)
+
+    def test_char_charset_digits(self):
+        digits = b'0123456789'
+        val = self.ser.serialize_value(self.ser.type_char, len(digits), digits)
+        self.assertEquals(val, digits)
+
+    def test_char_charset_special_chars(self):
+        specials = b"/-?().,+'{}: "
+        val = self.ser.serialize_value(self.ser.type_char, len(specials), specials)
+        self.assertEquals(val, specials)
+
+    def test_char_charset_outside_range(self):
+        # Try with some low bytes
+        with self.assertRaises(ValueError):
+            self.ser.serialize_value(self.ser.type_char, 3, b'\x00\x01')
+
+    def test_number_charset_too_long(self):
+        # Exceeds field length
+        with self.assertRaises(ValueError):
+            self.ser.serialize_value(self.ser.type_num, 3, 'ab3a')
+
+    # Numeric tests
+
+    def test_number_charset_value_range(self):
+        # All digits are accepted (and leading zero is preserved)
+        value_range = b'0123456789'
+        val = self.ser.serialize_value(self.ser.type_num, len(value_range), value_range)
+        self.assertEquals(val, value_range)
+
+    def test_number_charset_outside_range(self):
+        # Decimal point not allowed
+        with self.assertRaises(ValueError):
+            self.ser.serialize_value(self.ser.type_num, 10, '123.10')
+
+    def test_number_charset_too_long(self):
+        # Exceeds field length
+        with self.assertRaises(ValueError):
+            self.ser.serialize_value(self.ser.type_num, 3, '1234')
+
+    def test_number_charset_wrong_type(self):
+        # Try to write as characters
+        with self.assertRaises(ValueError):
+            self.ser.serialize_value(self.ser.type_num, 3, 'abc')
