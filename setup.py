761034e421ee78d6031a7ab1fc9b98b2b441870a
Martin Matusiak <numerodix@gmail.com>
2016-02-16 10:42:02 +0100

    fix trailing dash in version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='ginger-libmt94x',
     description='This library generates bank statements in MT940/MT942 format',
-    version=open('version.txt').read(),
+    version=open('version.txt').read().strip(),
     url='',
     packages=find_packages(),
     namespace_packages=['ginger'],
