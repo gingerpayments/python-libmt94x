b758b4d8649a058b6a14ce3ba4bf0c711876e884
Martin Matusiak <numerodix@gmail.com>
2016-05-31 09:24:16 +0200

    rewrite break_at_width to be more readable

--- ginger_libmt94x/textutil.py
+++ ginger_libmt94x/textutil.py
@@ -1,5 +1,6 @@
-def break_at_width(text, width=65, newline='\r\n'):
-    lines = []
+def break_at_width(text, width=None, newline=None):
+    width = width or 65
+    newline = newline or '\r\n'
 
     # If text is 130 chars + newline then we will produce:
     #   line1 + newline
@@ -9,17 +10,26 @@ def break_at_width(text, width=65, newline='\r\n'):
     if text.endswith(newline) and (len(text) - len(newline)) % width == 0:
         text = text[:-len(newline)]
 
-    iterations = len(text) / width
-    for i in range(iterations + 1):
-        line = text[i * width:(i + 1) * width]
-        if i < iterations:
+    # Figure out how many lines we will need to output
+    num_iterations = len(text) / width
+
+    # Chop the text into lines of length width and terminated with newline
+    lines = []
+    for idx in range(num_iterations + 1):
+        begin_pos = idx * width
+        end_pos = (idx + 1) * width
+        line = text[begin_pos:end_pos]
+
+        # If it's not the last line we need to append a newline
+        if idx < num_iterations:
             line = '%s%s' % (line, newline)
 
         lines.append(line)
 
+    # Re-assemble all the lines into a block of text
     block = ''.join(lines)
-    return block
 
+    return block
 
 def format_amount(amount, locale):
     if locale == 'nl_NL':
